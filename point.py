bf8fb105f033a637a32fc6043de445c101ac78ff
Fredrikvk2002 <fviksekarlse.ieu2023@student.ie.edu>
2025-03-14 13:22:02 +0100

    first lesson, struggled with syntax errors

---
+++ point.py
@@ -0,0 +1,48 @@
+Import random
+
+class Point:
+
+    """
+    Class modeling a real life 20 point
+    """
+
+    def __init__(self, x, y):
+        """"
+        Initialize the point instance
+        :param x: the x axis coordinate value
+        :param x: the x axis coordinate value
+        """
+        self.x = x
+        self.y = y
+
+
+
+    p1 = Point(x:1,y:1) #Create a new instance
+    print(p1.x, p1.y) #access attributes
+    print(p1)
+
+
+def __str__(self):
+    return f"<{self.x},{self.y}>"
+
+
+def __repr__(self):
+    return (self.x ** 2 + self.y ** 2) ** 0.5
+
+
+Points = []
+for i in range(5)
+    #Create random point
+    p = Point(
+        random.randint(-100,b:100)
+        random.randint(-100,b:100)
+    )
+    #append it to the list
+    Points.append(p)
+
+for point in points:
+
+print(points)
+#point.sort()
+p = point(-12,-5)
+print(p.distance_orig())
